3378d9b04467692c30d3047e03ac764be99f07df
Kirill Korolev <kirill.korolef@gmail.com>
2014-11-17 15:56:19 +0300

    [finished] Some visual corrections

--- task2/sources/task/iface.h
+++ task2/sources/task/iface.h
@@ -77,43 +77,43 @@ namespace Task //< Namespace for the programming task
             
         public:
             // INTERFACE -----------------------------------------------------------------------------------//
-            //Iterator's type for:
-            typedef my_iterator<EdIterT> pred_iterator; // node's predecessor edges
-            typedef my_iterator<EdIterT> succ_iterator; // node's successor edges
-             
-            // Get the iterator to:                                                                        //
-            inline pred_iterator preds_begin() { return _preds.begin(); } // the first predecessing edge  //
-            inline pred_iterator preds_end()   { return _preds.end();   } // the end of pred. list       //
-            inline succ_iterator succs_begin() { return _succs.begin(); } // the first successing edge  //
-            inline succ_iterator succs_end()   { return _succs.end();   } // the end of succ. list     //
-                                                                                                      //
-            inline Graph& graph() { return _graph; }  // Get the graph reference                     //
-            inline UId uid() const { return _id; }    // Get the node's unique id                   //
-                                                                                                   //
-            //Convinience routine returns:                                                        //
-            EdgeT &first_pred(); // first predecessor edge (else throws an exception)            //
-            EdgeT &first_succ(); // first successor edge (else throws an exception)             //
+            //Iterator's type for:                                                                         //
+            typedef my_iterator<EdIterT> pred_iterator; // node's predecessor edges                       //
+            typedef my_iterator<EdIterT> succ_iterator; // node's successor edges                        //
+                                                                                                        //
+            // Get the iterator to:                                                                    //
+            inline pred_iterator preds_begin() { return _preds.begin(); } // the first pred. edge     //
+            inline pred_iterator preds_end()   { return _preds.end();   } // the end of pred. list   //
+            inline succ_iterator succs_begin() { return _succs.begin(); } // the first succ. edge   //
+            inline succ_iterator succs_end()   { return _succs.end();   } // the end of succ. list //
+                                                                                                  //
+            inline Graph& graph() { return _graph; }  // Get the graph reference                 //
+            inline UId uid() const { return _id; }    // Get the node's unique id               //
                                                                                                //
-            // Get the number of:                                                             //
-            inline UInt32 num_preds() const { return _preds.size(); } // predecessors        //
-            inline UInt32 num_succs() const { return _succs.size(); } // successors         //
+            //Convinience routine returns:                                                    //
+            EdgeT &first_pred(); // first predecessor edge (else throws an exception)        //
+            EdgeT &first_succ(); // first successor edge (else throws an exception)         //
                                                                                            //
-        protected:                                                                        //
-            Node(Graph &g); // Constructor of the node                                   //
-            virtual ~Node() {};                                                         //
+            // Get the number of:                                                         //
+            inline UInt32 num_preds() const { return _preds.size(); } // predecessors    //
+            inline UInt32 num_succs() const { return _succs.size(); } // successors     //
                                                                                        //
-        private:                                                                      //
-            Node();               //<--TURNED------                                  //
-            Node(const Node &n);  //<---------OFF--                                 //
-            //---------------------------------------------------------------------//
+        protected:                                                                    //
+            Node(Graph &g); // Constructor of the node                               //
+            virtual ~Node() {};                                                     //
+                                                                                   //
+        private:                                                                  //
+            Node();               //<--TURNED------                              //
+            Node(const Node &n);  //<---------OFF--                             //
+            //-----------------------------------------------------------------//
             
-            // DATA -------------------------------------------------------------//
-            UId _id;                                                            //
-            NoIterT _iter; //< the iterator for _graph._nodes                  //
-            Graph &_graph;                                                    //
-            list<EdIterT> _preds;                                            //
-            list<EdIterT> _succs;                                           //
-            //-------------------------------------------------------------//
+            // DATA ---------------------------------------------------------//
+            UId _id;                                                        //
+            NoIterT _iter; //< the iterator for _graph._nodes              //
+            Graph &_graph;                                                //
+            list<EdIterT> _preds;                                        //
+            list<EdIterT> _succs;                                       //
+            //---------------------------------------------------------//
             
         };
 
@@ -145,44 +145,45 @@ namespace Task //< Namespace for the programming task
             Graph &_graph;                                           //
             NodeT &_pred;                                           //
             NodeT &_succ;                                          //
-            typename list<EdIterT>::iterator _pred_iter;                             //
-            typename list<EdIterT>::iterator _succ_iter;                            //
+            typename list<EdIterT>::iterator _pred_iter;          //
+            typename list<EdIterT>::iterator _succ_iter;         //
             //--------------------------------------------------//
         };
 
 
 
 
-        // INTERFACE ----------------------------------------------------------------------------------------//
-        //Iterator's type for:
-        typedef my_iterator<NodeT> node_iterator; // the graph's nodes
-        typedef my_iterator<EdgeT> edge_iterator; // the graph's edges
+        // INTERFACE -------------------------------------------------------------------------------------//
+        //Iterator's type for:                                                                           //
+        typedef my_iterator<NodeT> node_iterator; // the graph's nodes                                  //
+        typedef my_iterator<EdgeT> edge_iterator; // the graph's edges                                 //
+                                                                                                      //
+        // Get the iterator to:                                                                      //
+        inline node_iterator nodes_begin() { return _nodes.begin(); } // the first node             //
+        inline node_iterator nodes_end()   { return _nodes.end();   } // the nodes' end            //
+        inline edge_iterator edges_begin() { return _edges.begin(); } // the first edge           //
+        inline edge_iterator edges_end()   { return _edges.end();   } // the edges' end          //
+                                                                                                //
+        inline UInt32 num_nodes() const { return _nodes.size(); } // Get the number of nodes   //
+        inline UInt32 num_edges() const { return _edges.size(); } // Get the number of edges  //
+                                                                                             //
+        NodeT &create_node(); // Create a new node and return reference to it               //
+        EdgeT &create_edge(NodeT &pred, NodeT &succ); // Create an edge between nodes      //
+                                                                                          //
+        void remove(NodeT &node); // Remove and delete node                              //
+        void remove(EdgeT &edge); // Remove and delete edge                             //
+                                                                                       //
+        inline Graph() { _free_id.push_back(1); }                                     //
+        // Destructor, deletes all nodes and edges by list destructor:               //
+        virtual ~Graph() {}                                                         //
+        //-------------------------------------------------------------------------//
         
-        // Get the iterator to:                                                                             //
-        inline node_iterator nodes_begin() { return _nodes.begin(); } // the first node                    //
-        inline node_iterator nodes_end()   { return _nodes.end();   } // the nodes' end                   //
-        inline edge_iterator edges_begin() { return _edges.begin(); } // the first edge                  //
-        inline edge_iterator edges_end()   { return _edges.end();   } // the edges' end                 //
-                                                                                                       //
-        inline UInt32 num_nodes() const { return _nodes.size(); } // Get the number of nodes          //
-        inline UInt32 num_edges() const { return _edges.size(); } // Get the number of edges         //
-                                                                                                    //
-        NodeT &create_node(); // Create a new node and return reference to it                      //
-        EdgeT &create_edge(NodeT &pred, NodeT &succ); // Create a new edge between given nodes    //
-                                                                                                 //
-        void remove(NodeT &node); // Remove and delete node                                     //
-        void remove(EdgeT &edge); // Remove and delete edge                                    //
-                                                                                              //
-        inline Graph() { _free_id.push_back(1); }                                            //
-        virtual ~Graph() {} // Destructor, deletes all nodes and edges by list destructor   //
-        //---------------------------------------------------------------------------------//
-        
-        // DATA -------------------------------------------------------------------------//
-        list<UId> _free_id;                                                             //
-    private:                                                                           //
-        list<NodeT> _nodes;                                                           //
-        list<EdgeT> _edges;                                                          //
-        //--------------------------------------------------------------------------//
+        // DATA -----------------------------------------------------------------//
+        list<UId> _free_id;                                                     //
+    private:                                                                   //
+        list<NodeT> _nodes;                                                   //
+        list<EdgeT> _edges;                                                  //
+        //------------------------------------------------------------------//
     };
     
     
